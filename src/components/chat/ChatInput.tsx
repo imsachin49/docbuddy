7990bb8e984d1ed874f95a4c40e70edbb708e3ff
Sachin <kumaraditya7125@gmail.com>
2024-02-04 16:12:53 +0530

    chat/ai assistance done

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -9,14 +9,12 @@ interface ChatInputProps {
 }
 
 const ChatInput = ({ isDisabled }: ChatInputProps) => {
-  const { addMessage, handleInputChange, isLoading, message } =
-    useContext(ChatContext);
-
+  const { addMessage,handleInputChange,isLoading,message}=useContext(ChatContext);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex flex-col w-full flex-grow p-4">
             <div className="relative">
@@ -40,8 +38,7 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                 disabled={isLoading || isDisabled}
                 aria-label="send message"
                 className="absolute bottom-1.5 right-[8px]"
-                type="submit"
-                onClick={()=>{
+                onClick={() => {
                   addMessage();
                   textareaRef.current?.focus();
                 }}
@@ -49,9 +46,9 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                 <Send className="h-4 w-4" />
               </Button>
             </div>
-          </div> 
+          </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -4,33 +4,27 @@ import { Loader2, MessageSquare } from 'lucide-react'
 import Skeleton from 'react-loading-skeleton'
 import Message from './Message'
 import { useContext, useEffect, useRef } from 'react'
-import { ChatContext } from './ChatContext'
+import { ChatContext } from './ChatContext';
+import {useIntersection} from '@mantine/hooks';
 
 interface MessagesProps {
   fileId: string
 }
 
 const Messages = ({ fileId }: MessagesProps) => {
-  const { isLoading: isAiThinking }=useContext(ChatContext)
+  const { isLoading: isAiThinking }=useContext(ChatContext);
 
   const { data, isLoading, fetchNextPage } =
-    trpc.getFileMessages.useInfiniteQuery(
-      {
+    trpc.getFileMessages.useInfiniteQuery({
         fileId,
         limit: INFINITE_QUERY_LIMIT,
-      },
-      {
+      },{
         getNextPageParam: (lastPage) =>
           lastPage?.nextCursor,
         keepPreviousData: true,
-      }
-    )
+    });
 
-    console.log("data",data)
-
-  const messages = data?.pages.flatMap(
-    (page) => page.messages
-  )
+  const messages = data?.pages.flatMap((page) => page.messages)
 
   const loadingMessage = {
     createdAt: new Date().toISOString(),
@@ -47,34 +41,29 @@ const Messages = ({ fileId }: MessagesProps) => {
     ...(isAiThinking ? [loadingMessage] : []),
     ...(messages ?? []),
   ]
+  
+  const lastMessageRef = useRef<HTMLDivElement>(null);
 
-  // const lastMessageRef = useRef<HTMLDivElement>(null)
+  const { ref, entry } = useIntersection({
+    root: lastMessageRef.current,
+    threshold: 1,
+  })
 
-  // const { ref, entry } = useIntersection({
-  //   root: lastMessageRef.current,
-  //   threshold: 1,
-  // })
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage()
+    }
+  }, [entry, fetchNextPage])
 
-  // useEffect(() => {
-  //   if (entry?.isIntersecting) {
-  //     fetchNextPage()
-  //   }
-  // }, [entry, fetchNextPage])
-
-  return (
+  return(
     <div className='flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'>
       {combinedMessages && combinedMessages.length > 0 ? (
         combinedMessages.map((message, i) => {
-          const isNextMessageSamePerson =
-            combinedMessages[i - 1]?.isUserMessage ===
-            combinedMessages[i]?.isUserMessage
-
-            console.log("combinedMessages",combinedMessages);
-
+          const isNextMessageSamePerson=combinedMessages[i - 1]?.isUserMessage === combinedMessages[i]?.isUserMessage
           if (i === combinedMessages.length - 1) {
             return (
               <Message
-                // ref={ref}
+                ref={ref}
                 message={message}
                 isNextMessageSamePerson={
                   isNextMessageSamePerson
@@ -101,7 +90,6 @@ const Messages = ({ fileId }: MessagesProps) => {
           <Skeleton className='h-16' />
         </div>
       ) : (
-        console.log("combinedMessages",combinedMessages),
         <div className='flex-1 flex flex-col items-center justify-center gap-2'>
           <MessageSquare className='h-8 w-8 text-blue-500' />
           <h3 className='font-semibold text-xl'>
