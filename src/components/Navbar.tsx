4542df4525b51987a0ca472b11940acc64aa0e6e
Sachin <kumaraditya7125@gmail.com>
2024-02-05 12:37:57 +0530

    added stripe hook

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,76 +1,84 @@
-import React from "react";
-import MaxWidthWrapper from "./MaxWidthWrapper";
-import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
 import {
-  RegisterLink,
   LoginLink,
+  RegisterLink,
   getKindeServerSession,
-} from "@kinde-oss/kinde-auth-nextjs/server";
-import { ArrowRight } from "lucide-react";
+} from '@kinde-oss/kinde-auth-nextjs/server'
+import { ArrowRight } from 'lucide-react'
+import UserAccountNav from './UserAccountNav'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
-  const { getUser } = getKindeServerSession();
-  const user = getUser();
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
 
   return (
-    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
-        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link href="/" className="flex z-40 font-semibold text-xl">
-            <span>DocBuddy</span>
+        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+          <Link
+            href='/'
+            className='flex z-40 font-semibold'>
+            <span>Docbuddy</span>
           </Link>
-          {/* add mobile navbar */}
-          <div className="hidden items-center space-x-4 sm:flex">
-            <>
-              <Link
-                href="/pricing"
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm",
-                })}
-              >
-                Pricing
-              </Link>
-              {!user ? (
-                <>
-                  {" "}
-                  <LoginLink
-                    className={buttonVariants({
-                      variant: "ghost",
-                      size: "sm",
-                    })}
-                  >
-                    Sign In
-                  </LoginLink>
-                  <RegisterLink
-                    className={buttonVariants({
-                      size: "sm",
-                      className: "bg-orange-600 hover:bg-orange-500 text-white",
-                    })}
-                  >
-                    Get Started
-                    <ArrowRight className="ml-1.5 h-5 w-5" />
-                  </RegisterLink>{" "}
-                </>
-              ) : (
+
+          <MobileNav isAuth={!!user} />
+
+          <div className='hidden items-center space-x-4 sm:flex'>
+            {!user ? (
+              <>
                 <Link
-                  href="/dashboard"
+                  href='/pricing'
                   className={buttonVariants({
-                    // variant: "ghost",
-                    size: "sm",
-                    className: "rounded-full",
-                  })}
-                >
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: 'sm',
+                  })}>
+                  Get started{' '}
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  href='/dashboard'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
                   Dashboard
                 </Link>
-              )}
-            </>
+
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name
+                      ? 'Your Account'
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ''}
+                  imageUrl={user.picture ?? ''}
+                />
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
